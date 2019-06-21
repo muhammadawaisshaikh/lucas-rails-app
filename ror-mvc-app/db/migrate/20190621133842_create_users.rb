342d7d5cbbd3e78a4e72a375179c8d3c4cec31d5
Muhammad Awais <ashaikh@csquareonline.com>
2019-06-21 18:41:54 +0500

    user model created

---
+++ ror-mvc-app/db/migrate/20190621133842_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string, :name
+      t.integer, :mobile
+      t.string, :email
+      t.password, :password
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
