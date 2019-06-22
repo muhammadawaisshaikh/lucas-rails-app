cfbe7d727c6fe85bddd3c860e3fc6d610693db31
Muhammad Awais <ashaikh@csquareonline.com>
2019-06-22 19:07:39 +0500

    product_collection model created

---
+++ ror-mvc-app/db/migrate/20190622140713_create_product_collections.rb
@@ -0,0 +1,11 @@
+class CreateProductCollections < ActiveRecord::Migration[5.2]
+  def change
+    create_table :product_collections do |t|
+      t.integer, :product_id
+      t.integer, :user_id
+      t.string :collection_name
+
+      t.timestamps
+    end
+  end
+end
