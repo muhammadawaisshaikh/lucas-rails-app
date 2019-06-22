9d63d3cd82bd736b2419020d311dc27acbe15f4b
Muhammad Awais <ashaikh@csquareonline.com>
2019-06-22 19:12:04 +0500

    product_reviews model created

---
+++ ror-mvc-app/db/migrate/20190622141151_create_product_reviews.rb
@@ -0,0 +1,12 @@
+class CreateProductReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :product_reviews do |t|
+      t.integer, :product_id
+      t.integer, :user_id
+      t.string, :review
+      t.integer :star_rating
+
+      t.timestamps
+    end
+  end
+end
